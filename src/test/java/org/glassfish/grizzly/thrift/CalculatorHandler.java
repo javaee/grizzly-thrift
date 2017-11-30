8bd9e6554574877d159ba390f55a4ce4ed86487d
Bongjae Chang <bongjae.chang@gmail.com>
2017-11-30 17:07:41 +0900

    + Resolved "libthrfit dependency needs to be updated. #4" (https://github.com/javaee/grizzly-thrift/issues/4) + Updated the libthrift from v0.9.1 to v0.10.0

--- src/test/java/org/glassfish/grizzly/thrift/CalculatorHandler.java
+++ src/test/java/org/glassfish/grizzly/thrift/CalculatorHandler.java
@@ -72,31 +72,31 @@ public class CalculatorHandler implements Calculator.Iface {
 
   public int calculate(int logid, Work work) throws InvalidOperation {
     System.out.println("calculate(" + logid + ", {" + work.op + "," + work.num1 + "," + work.num2 + "})");
-    int val;
+    int val = 0;
     switch (work.op) {
-    case ADD:
-      val = work.num1 + work.num2;
-      break;
-    case SUBTRACT:
-      val = work.num1 - work.num2;
-      break;
-    case MULTIPLY:
-      val = work.num1 * work.num2;
-      break;
-    case DIVIDE:
-      if (work.num2 == 0) {
+      case ADD:
+        val = work.num1 + work.num2;
+        break;
+      case SUBTRACT:
+        val = work.num1 - work.num2;
+        break;
+      case MULTIPLY:
+        val = work.num1 * work.num2;
+        break;
+      case DIVIDE:
+        if (work.num2 == 0) {
+          InvalidOperation io = new InvalidOperation();
+          io.whatOp = work.op.getValue();
+          io.why = "Cannot divide by 0";
+          throw io;
+        }
+        val = work.num1 / work.num2;
+        break;
+      default:
         InvalidOperation io = new InvalidOperation();
-        io.what = work.op.getValue();
-        io.why = "Cannot divide by 0";
+        io.whatOp = work.op.getValue();
+        io.why = "Unknown operation";
         throw io;
-      }
-      val = work.num1 / work.num2;
-      break;
-    default:
-      InvalidOperation io = new InvalidOperation();
-      io.what = work.op.getValue();
-      io.why = "Unknown operation";
-      throw io;
     }
 
     SharedStruct entry = new SharedStruct();
